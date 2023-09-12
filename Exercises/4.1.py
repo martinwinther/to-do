91597162b441b8b46b23616a65ffd566815546f5
Martin Winther <martin.winther@protonmail.com>
2023-09-12 10:28:46 +0200

    day 4

---
+++ Exercises/4.1.py
@@ -0,0 +1,3 @@
+usd = input("How many dollars have you got? ")
+exchange_rate = 0.95
+print("The amount in euros is: " + str(int(usd) * exchange_rate))
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 todos = []
 
+commands = "add, show, edit or exit"
+
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type one of the following commands: " + commands + ": ")
 
     match user_action.strip():
         case 'add':
@@ -9,12 +11,29 @@ while True:
             todos.append(todo)
         case 'show' | 'snow':
             for item in todos:
-                print(item)
+                print(str(todos.index(item) + 1) + ". " + item)
+
+        case 'edit':
+            for item in todos:
+                print(str(todos.index(item) + 1) + ". " + item)
+            number = int(input("Which number do you want to edit? "))
+            existing_todo = todos[number - 1]
+            print("Number " + str(number) + " is " + existing_todo)
+            new_todo = input("Edit it to: ")
+            todos[number - 1] = new_todo
+            print("Edit saved!")
         case 'exit':
             break
         case _:
-            print("Please enter a correct command")
+            print("Please enter one of the following commands: " + commands + ": ")
+
+print("Here are your tasks: ")
+
+for item in todos:
+    print(str(todos.index(item) + 1) + ". " + item)
+
 print("Bye!")
+
 
 
 
