689c5c5fe8c65bd5f0456ef38cb190b4da6fba99
Martin Winther <martin.winther@protonmail.com>
2023-09-20 10:29:06 +0200

    day 12

---
+++ Exercises/12/12.1.py
@@ -0,0 +1,5 @@
+def liters_to_cubic(liters):
+    print(f"{liters / 1000}")
+
+
+liters_to_cubic(int(input("How many liters? ")))
--- main.py
+++ main.py
@@ -1,56 +1,56 @@
+COMMANDS = "add, show, edit, complete or exit"
+FILEPATH = 'todos.txt'
+
+
 def get_todos():
-    with open('todos.txt', 'r') as file_local:
+    with open(FILEPATH, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-commands = "add, show, edit, complete or exit"
+def write_todos(todos_arg):
+    # Check if the last item has a newline at the end, if not, add one
+    if todos and not todos[-1].endswith('\n'):
+        todos[-1] += '\n'
+    with open(FILEPATH, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+def print_todos(todos_arg):
+    for index, item in enumerate(todos_arg):
+        item = item.strip("\n")
+        print(f"{index + 1}. {item}")
+
 
 while True:
-    user_action = input("Type one of the following commands: " + commands + ": ")
+    user_action = input("Type one of the following commands: " + COMMANDS + ": ")
     user_action = user_action.strip()
 
     if user_action.startswith('add'):
         todo = user_action[4:]  # adds the input after the character with index 4
-
         todos = get_todos()
-            
-        # Check if the last item has a newline at the end, if not, add one
-        if todos and not todos[-1].endswith('\n'):
-            todos[-1] += '\n'
-
         todos.append(todo + '\n')
-
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos(todos)
 
     elif user_action.startswith('show'):
-
         todos = get_todos()
-
-        for index, item in enumerate(todos):
-            item = item.strip("\n")
-            print(f"{index+1}. {item}")
+        print_todos(todos)
 
     elif user_action.startswith('edit'):
         try:
             number = int(user_action[5:])
-
             todos = get_todos()
 
             # Check if the number is within the valid range
             if 1 <= number <= len(todos):
-                for index, item in enumerate(todos):
-                    item = item.strip("\n")
-                    print(f"{index+1}. {item}")
+                print_todos(todos)
 
                 existing_todo = todos[number - 1]
                 print("Number " + str(number) + " is " + existing_todo)
 
                 new_todo = input("Edit it to: ")
                 todos[number-1] = new_todo + '\n'
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todos)
+                write_todos(todos)
                 print("Edit saved!")
             else:
                 print("Your command is not valid")
@@ -66,8 +66,7 @@ while True:
             number = int(user_action[9:])
             todo_to_remove = todos[number - 1].strip('\n')
             todos.pop(number - 1)
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
 
             print(f"Good job! \n{todo_to_remove} was removed from the list.")
         except IndexError:
@@ -82,12 +81,9 @@ while True:
 
 print("Here are your tasks: ")
 
-
 todos = get_todos()
 
-for index, item in enumerate(todos):
-    item = item.strip("\n")
-    print(f"{index + 1}. {item}")
+print_todos(todos)
 
 print("Bye!")
 
